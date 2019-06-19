b152827dda2e150d40e8d16774558a97d3c54917
Dave Kelsey <David.Kelsey@gov.bc.ca>
2019-06-19 16:14:58 -0700

    add generated pipeline

---
+++ .pipeline/lib/config.js
@@ -0,0 +1,18 @@
+'use strict';
+const options= require('pipeline-cli').Util.parseArguments()
+const changeId = options.pr //aka pull-request
+const version = '1.0.0'
+const name = 'hello'
+
+const phases = {
+  build: {namespace:'devops-demo-tools'    , name: `${name}`, phase: 'build'  , changeId:changeId, suffix: `-build-${changeId}`  , instance: `${name}-build-${changeId}`  , version:`${version}-${changeId}`, tag:`build-${version}-${changeId}`},
+  dev: {namespace:'devops-demo-dev'    , name: `${name}`, phase: 'dev'  , changeId:changeId, suffix: `-dev-${changeId}`  , instance: `${name}-dev-${changeId}`  , version:`${version}-${changeId}`, tag:`dev-${version}-${changeId}`},
+};
+
+// This callback forces the node process to exit as failure.
+process.on('unhandledRejection', (reason) => {
+  console.log(reason);
+  process.exit(1);
+});
+
+module.exports = exports = {phases, options};

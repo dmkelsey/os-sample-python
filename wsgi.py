ac790595218e9b3e1a43c2cba380c893e4292232
Dave Kelsey <David.Kelsey@gov.bc.ca>
2019-06-21 12:00:20 -0700

    try again

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "<h1>Hello Bubba!</h1>"
+    return "<h2>Hello Bubba!</h2>"
 
 if __name__ == "__main__":
     application.run()

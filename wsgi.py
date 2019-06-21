c5c3cb24dc56cb49461c6fb1c87d9e3f8f098635
Dave Kelsey <David.Kelsey@gov.bc.ca>
2019-06-21 09:33:37 -0700

    expand h1

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello <h2>Bubba!</h2>"
+    return "<h1>Hello Bubba!</h1>"
 
 if __name__ == "__main__":
     application.run()

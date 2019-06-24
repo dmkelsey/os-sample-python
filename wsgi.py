21e5592640c8e16f797b9b3a1087e6a7b8e458a1
Dave Kelsey <David.Kelsey@gov.bc.ca>
2019-06-24 15:48:32 -0700

    change tag

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "<h2>Hello Bubba!</h2>"
+    return "<h1>Hello Bubba!</h1>"
 
 if __name__ == "__main__":
     application.run()

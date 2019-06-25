6718a3c92fa87ff4ba3d4ee3817ebd894b1056cc
dmkelsey <d.kelsey@yahoo.ca>
2019-06-25 09:38:59 -0700

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -5,7 +5,7 @@ application = Flask(__name__)
 @application.route("/")
 def hello():
     return """<h1>Hello Bubba!</h1>
-    <h2>from : <p style="color: blue;">%s</p> <h2>""" % socket.gethostname()
+    <h2>from : <p style="color: purple;">%s</p> <h2>""" % socket.gethostname()
 
 if __name__ == "__main__":
     application.run()
